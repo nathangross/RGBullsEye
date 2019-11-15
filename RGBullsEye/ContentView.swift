b93d3232173c6baecb0f055da431184bbc02b732
Nathan Gross <nathan@atomicrobot.com>
2019-11-15 14:10:12 -0500

    Initial Commit

---
+++ RGBullsEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RGBullsEye
+//
+//  Created by Nathan Gross on 11/15/19.
+//  Copyright © 2019 Nathan Gross. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
